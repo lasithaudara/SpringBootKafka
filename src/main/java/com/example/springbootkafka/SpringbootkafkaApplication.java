bea08eaf7873432ea32ae4296dd58917fd68c29f
lasithaudara <lasithaup@gmail.com>
2023-03-06 23:55:38 +0530

    Define Event and Create a KafkaProducer

--- src/main/java/com/example/springbootkafka/SpringbootkafkaApplication.java
+++ src/main/java/com/example/springbootkafka/SpringbootkafkaApplication.java
@@ -2,12 +2,19 @@ package com.example.springbootkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class SpringbootkafkaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootkafkaApplication.class, args);
+	}
+	@Bean
+	public String sendMsg(KafkaTemplate<String, String> kafkaTemplate){
+		kafkaTemplate.send("sampleKafkaTopic", "hello kafka");
+		return "test";
 	}
 
 }
